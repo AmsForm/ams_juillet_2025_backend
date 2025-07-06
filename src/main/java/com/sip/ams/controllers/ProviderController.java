00303017e5fc03d1433b72b30cb868f1a6bd0a7f
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-07-06 11:01:30 +0100

    ajout des Http.Status aux apis

--- src/main/java/com/sip/ams/controllers/ProviderController.java
+++ src/main/java/com/sip/ams/controllers/ProviderController.java
@@ -3,6 +3,8 @@ package com.sip.ams.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,19 +28,23 @@ public class ProviderController {
 	@Operation(summary = "Récupération de tous les providers")
 	@ApiResponses(value = {
 	    @ApiResponse(responseCode = "200", description = "Succès de get All"),
-	    @ApiResponse(responseCode = "404", description = "Provider non trouvé")
+	    @ApiResponse(responseCode = "500", description = "Problème lors de la récupération")
 	})
-	public List<Provider> getAllProviders()
+	public ResponseEntity<List<Provider>> getAllProviders()
 	{
-		return (List<Provider>)providerRepository.findAll();
+		return new ResponseEntity<>((List<Provider>)providerRepository.findAll(),HttpStatus.CREATED);
 	}
 	
 	
 	@PostMapping("/")
-	public Provider saveProvider(@RequestBody Provider p)
+	@Operation(summary = "Ajout d'un nouvel provider")
+	@ApiResponses(value = {
+	    @ApiResponse(responseCode = "201", description = "Insertion avec succès"),
+	    @ApiResponse(responseCode = "500", description = "Problème lors de l'insertion")
+	})
+	public  ResponseEntity<Provider> saveProvider(@RequestBody Provider p)
 	{
-		
-		return providerRepository.save(p);
+		return new ResponseEntity<>(providerRepository.save(p),  HttpStatus.CREATED);
 	}
 	
 
