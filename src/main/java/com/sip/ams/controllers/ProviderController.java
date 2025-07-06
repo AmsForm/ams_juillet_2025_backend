7e9acfebb43eb08cccd340c1b055550c61726883
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-07-06 10:42:04 +0100

    ajout de swagger et optimisation de getAllProviders

--- src/main/java/com/sip/ams/controllers/ProviderController.java
+++ src/main/java/com/sip/ams/controllers/ProviderController.java
@@ -5,28 +5,39 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sip.ams.entities.Provider;
 import com.sip.ams.repositories.ProviderRepository;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 @RestController
+@RequestMapping("providers")
 public class ProviderController {
 	
 	@Autowired
 	ProviderRepository providerRepository;
 	
-	@GetMapping("/providers")
+	@GetMapping("/")
+	@Operation(summary = "Récupération de tous les providers")
+	@ApiResponses(value = {
+	    @ApiResponse(responseCode = "200", description = "Succès de get All"),
+	    @ApiResponse(responseCode = "404", description = "Provider non trouvé")
+	})
 	public List<Provider> getAllProviders()
 	{
 		return (List<Provider>)providerRepository.findAll();
 	}
 	
 	
-	@PostMapping("/provider")
-	public Provider saveProvider()
+	@PostMapping("/")
+	public Provider saveProvider(@RequestBody Provider p)
 	{
-		Provider p = new Provider("HP","hp@gmail.com","USA");
+		
 		return providerRepository.save(p);
 	}
 	
